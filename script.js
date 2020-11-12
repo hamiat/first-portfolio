8d5bba10c0f46ad638e62fbb3772708b2dd4d8e5
hamiat <70755521+hamiat@users.noreply.github.com>
2020-11-12 19:20:46 +0100

    Added more CSS

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let projectsBtn = document.querySelector('#my-projects');
 let aboutBtn = document.querySelector('#about-me');
 let contactBtn = document.querySelector('#contact-me');
 let projects = document.querySelector('.projects');
+let projectsHeader = document.querySelector('#my-projects');
+let projectsSections = document.querySelector('.projects-head');
 let aboutMe = document.querySelector('.about-me-text');
+let aboutHeader = document.querySelector('#about-me');
+let aboutSection = document.querySelector('.about');
 let contactForm = document.querySelector('.contact-form');
+let contactHeader = document.querySelector('#contact-me');
+let contactSection = document.querySelector('#contact');
 
 projectsBtn.addEventListener('click', showProjects); 
 aboutBtn.addEventListener('click', showAbout);
@@ -12,14 +18,20 @@ contactBtn.addEventListener('click', showContact);
 
 function showProjects () {
     projects.style.display = "inline-block";
+    projectsHeader.classList.add('projects-heading');
+    projectsSections.classList.add('projects-paint')
 }
 
 function showAbout() {
     aboutMe.style.display = "flex";
+    aboutHeader.classList.add('about-heading');
+    aboutSection.classList.add('about-paint');
     
 }
 
 function showContact() {
     contactForm.style.display =  "grid";
+    contactHeader.classList.add('contact-heading');
+    contactSection.classList.add('contact-paint');
 
 }
